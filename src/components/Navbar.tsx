76ea55f27941319e8c7d7f60c61c04e4be66330e
AahilRafiq <aahil.rafiq136@gmail.com>
2025-05-20 09:38:20 +0530

    make navbar smaller

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,17 +20,17 @@ export default function ({theme, setTheme}:IProps) {
     }
 
     return (
-        <div className="navbar bg-base-100 shadow-sm h-2">
+        <div className="flex flex-row p-1 place-items-center bg-base-100 shadow-sm">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Excalidraw+Icons</a>
+                <a className="btn btn-ghost text-lg ">Excalidraw+Icons</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><button className="btn mx-2 btn-ghost rounded-full" onClick={handleThemeToggle}>
+                    <li><button className="btn btn-sm mx-2 btn-ghost rounded-full" onClick={handleThemeToggle}>
                         {theme == "dark" ? <Sun/> : <Moon/>}
                     </button></li>
-                    <li><button onClick={() => setOpenModal(true)} className="btn btn-soft btn-primary mr-2">+ Add Icon</button></li>
-                    <li><a href="https://github.com/AahilRafiq/excalidraw-plus-icons" className="btn btn-outline ml-2"><Github/></a></li>
+                    <li><button onClick={() => setOpenModal(true)} className="btn btn-sm btn-soft btn-primary mr-2">+ Add Icon</button></li>
+                    <li><a href="https://github.com/AahilRafiq/excalidraw-plus-icons" className="btn btn-sm btn-outline ml-2"><Github/></a></li>
                 </ul>
             </div>
             <IconModal openModal={openModal} setOpenModal={setOpenModal}/>
